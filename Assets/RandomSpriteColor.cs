7a4dc54593d1742bc2ee576c73b82a6703b9b941
Guthmann <t.guthmann@rubika-edu.com>
2017-03-07 11:06:40 +0100

    Chunks

--- Assets/RandomSpriteColor.cs
+++ Assets/RandomSpriteColor.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class RandomSpriteColor : MonoBehaviour {
 
     public Gradient colors;
+    public float speed;
 
 	// Use this for initialization
 	void Start ()
     {
-        GetComponent<SpriteRenderer>().color = colors.Evaluate(Mathf.Abs((Mathf.Sin(Time.time)+1)/2));
+        GetComponent<SpriteRenderer>().color = colors.Evaluate(Mathf.Abs((Mathf.Sin(Time.time *speed)+1)/2));
 	}
 	
 	// Update is called once per frame
