de4eb15304226804df12d54017e2e342e9826ec7
Guthmann <t.guthmann@rubika-edu.com>
2017-03-09 11:33:17 +0100

    aaa

---
+++ Assets/OpacityFromParent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpacityFromParent : MonoBehaviour {
+
+    public Image[] images;
+
+	// Use this for initialization
+	void Start ()
+    {
+        images = GetComponentsInParent<Image>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        images[0].color = new Color(images[0].color.r, images[0].color.g, images[0].color.b, images[1].color.a);
+	}
+}
